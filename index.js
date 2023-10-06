18a8df93f827f8c993ca4ed2494acf922a94374c
Abbatronics <125681774+Abbatronics@users.noreply.github.com>
2023-10-06 20:50:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,8 +53,24 @@ btnEl.addEventListener("click", () => {
         <br>
         <strong style="color:white ;">Feedback: ${selectedRating}</strong>
         <p>We'll use your feedback to improve our customer support.</p>
-        `;
+        ;
   }
 });
 
 }
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEl.style.width = size + "px";
+  spanEl.style.height = size + "px";
+  bodyEl.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+});
