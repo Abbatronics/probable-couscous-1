9114714e040a7c60cb79909720e669929a49e80c
Abbatronics <125681774+Abbatronics@users.noreply.github.com>
2023-10-06 20:17:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -56,14 +56,5 @@ btnEl.addEventListener("click", () => {
         `;
   }
 });
-const bar = document.querySelector(".fa-bars");
-const menu = document.querySelector(".menu");
 
-bar.addEventListener("click", () => {
-  menu.classList.toggle("show-menu");
-});
-function removeActive() {
-  ratingEls.forEach((ratingEl) => {
-    ratingEl.classList.remove("active");
-  });
 }
